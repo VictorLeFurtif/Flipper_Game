910bb8d36eb623fcb311c6e60337f7620e856420
Victor <victor.baz92@gmail.com>
2024-10-29 13:54:17 +0100

    NewMenu

--- BAZ Victor Flipper V2/Assets/Scripts/DeathZone.cs
+++ BAZ Victor Flipper V2/Assets/Scripts/DeathZone.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 public class DeathZone : MonoBehaviour
 {
    
--- BAZ Victor Flipper V2/Assets/Scripts/LoadSceneGame.cs
+++ BAZ Victor Flipper V2/Assets/Scripts/LoadSceneGame.cs
@@ -7,7 +7,7 @@ public class LoadSceneGame : MonoBehaviour
 {
    public void LoadsceneByName()
    {
-      SceneManager.LoadScene("Tuto");
+      SceneManager.LoadScene("Menu_Level");
       Time.timeScale = 1;
    }
 
