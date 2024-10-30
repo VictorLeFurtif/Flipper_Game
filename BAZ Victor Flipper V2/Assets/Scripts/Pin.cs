98e5a7b5d81e86eb0288fbe6bc1360cd4774e5de
Victor <victor.baz92@gmail.com>
2024-10-30 17:31:19 +0100

    music button commit

--- BAZ Victor Flipper V2/Assets/Scripts/Pin.cs
+++ BAZ Victor Flipper V2/Assets/Scripts/Pin.cs
@@ -8,6 +8,8 @@ using UnityEngine.EventSystems;
 public class Pin : MonoBehaviour
 {
     public Animation anim;
+    public AudioSource audioButton;
+    
     void Start()
     {
         gameObject.SetActive(false);
@@ -17,6 +19,7 @@ public class Pin : MonoBehaviour
     public void OnPointerEnter(PointerEventData eventData)
     {
         gameObject.SetActive(true);
+        audioButton.Play();
     }
 
     public void Update()
