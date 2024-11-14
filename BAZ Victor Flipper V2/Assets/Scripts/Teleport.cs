234d928ba9ce741ffcbc43d949e7eba955a488c1
Victor <victor.baz92@gmail.com>
2024-11-14 16:40:36 +0100

    commit jsp

--- BAZ Victor Flipper V2/Assets/Scripts/Teleport.cs
+++ BAZ Victor Flipper V2/Assets/Scripts/Teleport.cs
@@ -9,11 +9,12 @@ public class Teleport : MonoBehaviour
     public GameObject objectWhereToTp;
     public Animation animCam;
     public GameObject ball;
-    
-    
+
+
     void OnTriggerEnter(Collider other)
     {
-        other.transform.position = new Vector3(objectWhereToTp.transform.position.x, objectWhereToTp.transform.position.y, objectWhereToTp.transform.position.z);
+        other.transform.position = new Vector3(objectWhereToTp.transform.position.x,
+            objectWhereToTp.transform.position.y, objectWhereToTp.transform.position.z);
         other.GetComponent<Rigidbody>().velocity = Vector3.zero;
         other.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
         other.gameObject.GetComponent<MeshRenderer>().enabled = false;
@@ -24,29 +25,27 @@ public class Teleport : MonoBehaviour
 
     IEnumerator WaitForTimeScale(Collider other)
     {
-        
         yield return new WaitForSecondsRealtime(1);
         other.gameObject.GetComponent<MeshRenderer>().enabled = true;
         other.gameObject.GetComponent<Rigidbody>().isKinematic = false;
-        other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-3,3),0,0),ForceMode.Impulse);
-        
+        other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-3, 3), 0, 0), ForceMode.Impulse);
     }
+
     IEnumerator WaitForTimeScaleTwo(GameObject other)
     {
-        
         yield return new WaitForSecondsRealtime(1);
         other.gameObject.GetComponent<MeshRenderer>().enabled = true;
         other.gameObject.GetComponent<Rigidbody>().isKinematic = false;
-        other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-3,3),0,0),ForceMode.Impulse);
-        
+        other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-3, 3), 0, 0), ForceMode.Impulse);
     }
-    
+
 
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.B))
         {
-            ball.transform.position = new Vector3(objectWhereToTp.transform.position.x, objectWhereToTp.transform.position.y, objectWhereToTp.transform.position.z);
+            ball.transform.position = new Vector3(objectWhereToTp.transform.position.x,
+                objectWhereToTp.transform.position.y, objectWhereToTp.transform.position.z);
             ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
             ball.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
             ball.gameObject.GetComponent<MeshRenderer>().enabled = false;
